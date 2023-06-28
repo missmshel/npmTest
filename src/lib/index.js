cb16eca210b638df48b8fe0c6e8de14a28c13dc1
missmshel <mshelconroy@gmail.com>
2023-06-27 20:31:04 -0600

    test npm package first try

---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+// Reexport your entry components here
+import Glitter from "./svelte/glitter.svelte";
+
+export { Glitter };
---
+++ src/lib/svelte/glitter.svelte
@@ -0,0 +1,5 @@
+<script lang="ts"> 
+    let name: string = 'Hello World! I am taking over!'
+</script>
+
+<h1>{name}</h1>
